38fecd9a8bd6e3f51613cf4dd0a01cbcebdca32c
Name <anushqakandan03@gmail.com>
2024-08-07 12:44:05 +0200

    Folder Structure

---
+++ config/index.js
@@ -0,0 +1,17 @@
+import {createPool} from mysql2
+import 'dotenv/config'
+let connection = createPool({
+    host: process.env.hostDb,
+    user: process.env.userDb,
+    password: process.env.pwdDb,
+    mutipleStatements: true,
+    connectionLimit: 30, 
+})
+
+connection.on('connection', (pool) => {
+    if (!pool) throw new Error('Couldn\'t connect to the database, please try again later')
+    })
+
+    export{
+        connection
+    }
